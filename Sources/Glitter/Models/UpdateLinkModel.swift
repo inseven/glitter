aa9588227144146f41d7422c23212b776babef5c
Jason Morley <hello@jbmorley.co.uk>
2026-04-20 10:34:46 -1000

    fix: Constrain `UpdateLinkModel` to `MainActor`
    This is required for later SDKs and builds of Sparkle.

--- Sources/Glitter/Models/UpdateLinkModel.swift
+++ Sources/Glitter/Models/UpdateLinkModel.swift
@@ -25,6 +25,7 @@ import SwiftUI
 import Sparkle
 
 // This view model class publishes when new updates can be checked by the user
+@MainActor
 final class UpdateLinkModel: ObservableObject {
     @Published var canCheckForUpdates = false
 
